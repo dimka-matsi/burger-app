d81563b3679164d13d3f657dd4a3cea3d480b677
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-10-10 22:00:10 +0300

    update Burger App

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
